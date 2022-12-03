23bc28dc8f14e15fc193a04abe2c94e748f50749
Dmitri S. Cunev <cunevnew@gmail.com>
2022-12-04 00:45:29 +0200

    Resize images

--- src/canvas/elements/group.ts
+++ src/canvas/elements/group.ts
@@ -103,7 +103,7 @@ export class Group extends Block {
 		this.texture.pop();
 	}
 
-	handleDragStart(): void {}
+	handleDragStart(): boolean | void {}
 
 	handleMouseRelease(): void {}
 }
--- src/canvas/elements/image.ts
+++ src/canvas/elements/image.ts
@@ -1,5 +1,6 @@
 import p5 from "p5";
 import { addAssetRaw, assets } from "../primitives/assets";
+import { Camera } from "../primitives/camera";
 import { Vector2 } from "../primitives/math";
 import { p } from "../primitives/p5";
 import { handText } from "../primitives/textGenerator";
@@ -9,9 +10,12 @@ export type TaskPriority = "S" | "M" | "L" | "XL";
 export class ImageBlock extends Block {
 	type = BlockType.Image;
 	order: number = 2;
-	size: Vector2 = { x: 400, y: 110 };
-
+	ratio: number = 1;
+	scale: number = 1;
+	size: Vector2 = { x: 0, y: 0 };
 	imageData: string = "";
+
+	isResizing: boolean = false;
 
 	constructor() {
 		super();
@@ -20,6 +24,10 @@ export class ImageBlock extends Block {
 	draw() {
 		p.push();
 		this.drawSelection();
+		if (this.isResizing) {
+			this.size.x = Camera.mouseX - this.position.x;
+			this.size.y = this.size.x / this.ratio;
+		}
 		if (assets.has(this.id)) {
 			p.image(
 				assets.get(this.id)!,
@@ -28,6 +36,7 @@ export class ImageBlock extends Block {
 				this.size.x,
 				this.size.y
 			);
+			this.drawScaleButton();
 		} else if (this.imageData) {
 			this.attachFile(this.imageData);
 		}
@@ -42,12 +51,57 @@ export class ImageBlock extends Block {
 		addAssetRaw(this.id, image);
 	}
 
+	drawScaleButton() {
+		if (!this.isSelected) return;
+		p.push();
+		p.noStroke();
+		p.fill("#6965db");
+		p.triangle(
+			this.position.x + this.size.x,
+			this.position.y + this.size.y - 15,
+			this.position.x + this.size.x,
+			this.position.y + this.size.y,
+			this.position.x + this.size.x - 15,
+			this.position.y + this.size.y
+		);
+		if (
+			this.cursorInside() &&
+			p.dist(
+				this.position.x + this.size.x,
+				this.position.y + this.size.y,
+				Camera.mouseX,
+				Camera.mouseY
+			) < 15
+		) {
+			p.cursor("nwse-resize");
+		}
+		p.pop();
+	}
+
 	onImageLoad(image: p5.Image) {
-		this.size.x = image.width;
-		this.size.y = image.height;
+		this.ratio = image.width / image.height;
+		if (!this.size.x && !this.size.y) {
+			this.size.x = image.width;
+			this.size.y = image.height;
+		}
 	}
 
 	renderTexture(): void {}
-	handleDragStart(): void {}
-	handleMouseRelease(): void {}
+	handleDragStart(): boolean | undefined {
+		if (
+			this.cursorInside() &&
+			p.dist(
+				this.position.x + this.size.x,
+				this.position.y + this.size.y,
+				Camera.mouseX,
+				Camera.mouseY
+			) < 15
+		) {
+			this.isResizing = true;
+			return true;
+		}
+	}
+	handleMouseRelease(): void {
+		this.isResizing = false;
+	}
 }
